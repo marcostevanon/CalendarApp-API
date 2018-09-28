9cf9bc2b74c4782a45ee171a02762bb560fd8154
Marco Stevanon <marco.stevanon@gmail.com>
2018-09-28 15:44:57 +0200

    fix workflow

--- index.js
+++ index.js
@@ -1,10 +1,86 @@
 "use strict";
 const app = require('express')();
 const morgan = require('morgan');
+const cors = require('cors');
+const mysql = require('mysql');
+const moment = require('moment');
+moment.locale('it');
+var db = mysql.createPool(require(__dirname + '/config.js').mysql);
 
+app.use(cors())
 app.use(morgan('combined'));
 
-app.use('/times', require(__dirname + '/routes/times.js'));
+app.get('/course/list', (req, res) => {
+    var query = `SELECT name, csvCode, year FROM Course WHERE active = 1 ORDER BY year`;
+
+    db.query(query, [], (err, rows) => {
+        if (err) res.sendStatus(500);
+        res.json(rows);
+    });
+});
+
+app.get('/course/:course', (req, res) => {
+    var query = `SELECT * FROM Course WHERE csvCode = ? AND active = 1`;
+    var args = [];
+
+    if (req.params.course) {
+        args.push(req.params.course);
+
+        db.query(query, args, (err, rows) => {
+            if (err) { res.sendStatus(500); }
+            res.json(rows[0]);
+        });
+    } else { res.sendStatus(400); }
+});
+
+app.get('/lastupdt/:course', (req, res) => {
+    var query = `SELECT max(id) as id, date FROM log WHERE courseCode = ?`;
+    var args = [];
+
+    if (req.params.course) {
+        args.push(req.params.course);
+
+        db.query(query, args, (err, rows) => {
+            if (err) { res.sendStatus(500); }
+            res.json(rows[0].date);
+        });
+    } else { res.sendStatus(400); }
+});
+
+app.get('/times/:course', (req, res) => {
+    var query = `SELECT
+            Times.id         as id,
+            Times.webID      as webID,
+            Course.name      as courseName,
+            Times.moduleName as moduleName,
+            Times.required   as required,
+            Professor.name   as prof,
+            Room.name        as room,
+            Building.name    as building,
+            Times.date       as date,
+            Times.timestart  as timestart,
+            Times.timeend    as timeend,
+            Times.note       as note
+        FROM Times
+            JOIN Course ON Times.id_course = Course.id
+            JOIN Professor ON Times.id_professor = Professor.id
+            JOIN Room ON Times.id_room = Room.id
+            JOIN Building ON Room.id_building = Building.id
+        WHERE Course.csvCode = ? AND Times.date >= ?`;
+    var args = [];
+    
+    if (req.params.course) {
+        args.push(req.params.course);
+
+        if (req.query.date) { args.push(req.query.date); }
+        else { args.push(moment().format('YYYY-MM-DD')); }
+
+        db.query(query, args, (err, rows) => {
+            if (err) { res.sendStatus(500); }
+            res.json(rows);
+        });
+    } else { res.sendStatus(400); }
+});
 
 app.all('*', (req, res) => {
     res.sendStatus(404);
